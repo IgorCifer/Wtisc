7025cab9d37103106628f75bb13c968df93feaa9
Igor Farias <fariasigor547@gmail.com>
2024-10-12 16:29:54 -0300

    Sparkle:: Front

---
+++ wtisc-teste-front/client/src/lib/apiRequest.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+const apiRequest = axios.create({
+  baseURL: "https://wtisc1.up.railway.app",
+  withCredentials: true
+});
+
+export default apiRequest;
+
+//https://wtisc1.up.railway.app
---
+++ wtisc-teste-front/client/src/routes/leacturePage/leacturePage.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import LectureDetails from '../../components/leactures/LectureDetails'; // O componente que criamos
+
+const LecturePage = () => {
+  const { id } = useParams(); // Esse "id" é o lectureId
+  const [lecture, setLecture] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchLectureDetails = async () => {
+      try {
+        const response = await fetch(`https://wtisc1.up.railway.app/lectures/${id}`);
+        
+        if (!response.ok) {
+          throw new Error(`Erro na resposta da API: ${response.statusText}`);
+        }
+    
+        const data = await response.json();
+        setLecture(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Erro ao buscar detalhes da palestra:', error);
+        setLoading(false);
+      }
+    };
+    
+
+    fetchLectureDetails();
+  }, [id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (!lecture) {
+    return <p>Palestra não encontrada.</p>;
+  }
+
+  return (
+    <LectureDetails
+      title={lecture.title}
+      description={lecture.description}
+      speaker={lecture.speaker}
+      date={lecture.date}
+      lectureLocation={lecture.location} // Passando a localização corretamente
+      enrolled={lecture.enrolled}
+      capacity={lecture.capacity}
+      lectureId={id} // Passando o ID da palestra como prop
+    />
+  );
+};
+
+export default LecturePage;
